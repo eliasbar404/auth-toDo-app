123c593f3d7b1dfba165fd11221f69d98dae5846
ilyas <ibaraaouche@gmail.com>
2024-02-11 11:45:21 +0100

    setup frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+  
+  return (
+    <div>
+      <h1 className="bg-red-300">Auth To Do App</h1>
+      <Button>Click me</Button>
+
+    </div>
+  )
+}
+
+export default App
